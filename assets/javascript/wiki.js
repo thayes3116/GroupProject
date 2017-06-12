b3a89b04ce348c422ad49c3208512cfd06288ebd
thayes3116 <timhayes3116@gmail.com>
2017-06-12 12:23:48 -0400

    added wiki js file

---
+++ assets/javascript/wiki.js
@@ -0,0 +1,8 @@
+// var queryURL5 = "https://en.wikipedia.org/w/api.php?action=query&titles=Austin&prop=info&format=jsonfm";
+// 					$.ajax({
+//                         url: queryURL5,
+//                         method: "GET"
+//                     }).done(function(response) {
+//                     		console.log(response)
+//                     		console.log("hi")
+//                     });	
